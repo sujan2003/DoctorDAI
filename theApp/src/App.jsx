27f0bfba635ff94a77bd346195a2d1ece6f70779
sujan2003 <niroulasujan7@gmail.com>
2024-04-11 11:07:40 -0500

    add auto scrolling feature

--- theApp/src/App.jsx
+++ theApp/src/App.jsx
@@ -7,13 +7,13 @@ import logo from './assets/logo.webp'
 
 function App() {
   return (
-    <>
+    <> 
       <span className='centering'>
         <div className=" w-85">
           <span className="centering">
             <img className='rounded-full' width='100px' height='100px' src={logo} alt="logo" />
           </span>
-          <h1 className="font-bold text-2xl">Chat with DocDAI 👨🏽‍⚕️</h1>
+          <h1 className="font-bold text-2xl">Chat with DocDAI!</h1>
           <h5>Your go-to companion for navigating health information.</h5>
         </div>
       </span>
